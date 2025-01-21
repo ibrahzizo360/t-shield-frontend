fba481937fdd1d69c018062cf82ebdba25d93c87
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2025-01-21 02:06:23 +0000

    changed url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000';
-// export const API_URL = 'https://t-shield-backend.onrender.com';
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://t-shield-backend.onrender.com';
